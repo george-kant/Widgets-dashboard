1b40655f958c0659f290b21c94ef36e7eb648baa
Andreas Christou <anc.christou@edu.cut.ac.cy>
2024-10-17 19:31:46 +0300

    yyhy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    " building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write( " ig For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
