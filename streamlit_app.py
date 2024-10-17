3f4288aa74ff1dde5390459b90fc172472dd1446
George Kantianis <kantianisgeorge@outlook.com>
2024-10-17 19:46:48 +0300

    Random facts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import requests
+# Send a GET request to the API
+response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
 
-st.title("🎈 My new app")
+st.title("🎈 Widgets App Dashboard")
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the JSON response
+    fact = response.json()
+    # Print the fact
+    print(fact["text"])
+else:
+    st.write(f"Failed to retrieve fact. Status code: {response.status_code}")
+
 st.write( " George ig For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
