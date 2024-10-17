b02605956cba5745478ad9dbec526431e9e7e07d
George Kantianis <kantianisgeorge@outlook.com>
2024-10-17 19:34:39 +0300

    George commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write( " ig For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.write( " George ig For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
